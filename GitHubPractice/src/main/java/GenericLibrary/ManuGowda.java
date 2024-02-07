c477524c201dc8091ea7e35fe2cf95bfa3994c86
Monish S N <Monish S N@TYSS-Monish-S-N>
2024-02-07 17:15:57 +0530

    First Commit

---
+++ GitHubPractice/src/main/java/GenericLibrary/ManuGowda.java
@@ -0,0 +1,19 @@
+package GenericLibrary;
+
+public class ManuGowda {
+
+	String name="ManuGowda";
+	String id="TYP2652";
+	String BloodGroup="A+";
+	
+	public void playCricket() {
+		System.out.println("Play Cricket");
+	}
+	public void EatNonVeg() {
+		System.out.println("Eat Biriyani");
+	}
+	public void LearnSelenium() {
+		System.out.println("Learn Selenium");
+	}
+
+}
